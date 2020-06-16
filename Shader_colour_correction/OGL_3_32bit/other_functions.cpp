2bb6fc8bb8b36abeb59e4d4ff1d30b70cf7883ea
alexander_k <43931688+allex-k@users.noreply.github.com>
2020-06-16 22:09:18 +0300

    Delete other_functions.cpp

--- Shader_colour_correction/OGL_3_32bit/other_functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "other_functions.h"
-#define _USE_MATH_DEFINES
-#include <math.h>
-
-void print_array(float* arr, int n) {
-	for (int i = 0; i < n; ++i) {
-		std::cout << "arr[" << i << "] = " << arr[i] << std::endl;
-	}
-}
